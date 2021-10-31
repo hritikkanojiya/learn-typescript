b8e8698ffd8476e0aade775d1e9f1109ab6ab659
Hritik <hritikkanojiya13@gmail.com>
2021-10-31 22:22:44 +0530

    Created a function to add numbers in TS

---
+++ script.js
@@ -0,0 +1,3 @@
+var add = function (val1, val2) { return val1 + val2; };
+var result = add(5, 2);
+console.log(result);
---
+++ script.ts
@@ -0,0 +1,3 @@
+const add = (val1: number, val2: number):number => val1 + val2;
+var result = add(5, 2);
+console.log(result);
