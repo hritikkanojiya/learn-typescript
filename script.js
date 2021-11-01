f0acfe96730f6b40d7545af2fe0674c15617cd0b
Hritik <hritikkanojiya13@gmail.com>
2021-11-01 12:45:05 +0530

    TypeScript Data Types (number,string,array,object)

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-var add = function (val1, val2) { return val1 + val2; };
+var add = function (val1, val2) {
+  return val1 + val2;
+};
 var result = add(5, 2);
 console.log(result);
+/*
+ * TypeScript Data Types
+ * Syntax => variableName: dataType = value;
+ * Datatype should be lowercase
+ */
+// number => Int,Float
+var countOfMinions = 3;
+// string => Anything with ''
+var minion1 = "Kevin";
+var minion2 = "Stuart";
+var minion3 = "Bob";
+// array of srings
+var minions = ["Kevin", "Stuart", "Bob"];
+// Object with a defined structure
+var minionsObj;
+// Delcaring Object
+minionsObj = {
+  talent: ["Leadership"],
+  name: "Kevin",
+  height: "Tall",
+  age: 10,
+  isSmart: true,
+};
+console.log(minionsObj["name"]);
+// for array of Objects use [] after array structure initialization
+var minionsObjArray;
--- script.ts
+++ script.ts
@@ -1,3 +1,49 @@
-const add = (val1: number, val2: number):number => val1 + val2;
+const add = (val1: number, val2: number): number => val1 + val2;
 var result = add(5, 2);
 console.log(result);
+
+/*
+ * TypeScript Data Types
+ * Syntax => variableName: dataType = value;
+ * Datatype should be lowercase
+ */
+
+// number => Int,Float
+let countOfMinions: number = 3;
+
+// string => Anything with ''
+let minion1: string = "Kevin";
+let minion2: string = "Stuart";
+let minion3: string = "Bob";
+
+// array of srings
+let minions: string[] = ["Kevin", "Stuart", "Bob"];
+
+// Object with a defined structure
+let minionsObj: {
+  name: string;
+  talent: string[];
+  height: string;
+  age: number;
+  isSmart: boolean;
+};
+
+// Delcaring Object
+minionsObj = {
+  talent: ["Leadership"],
+  name: "Kevin",
+  height: "Tall",
+  age: 10,
+  isSmart: true,
+};
+
+console.log(minionsObj['name']);
+
+// for array of Objects use [] after array structure initialization
+let minionsObjArray: {
+  name: string;
+  talent: string[];
+  height: string;
+  age: number;
+  isSmart: boolean;
+}[];
